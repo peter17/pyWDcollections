172ba2158f875c703b1917e9c8c2fb97a8aedbcc
Peter Potrowl <peter.potrowl@gmail.com>
2016-03-22 22:07:03 +0100

    Save Wikidata items to the database.

--- pywdcollections.py
+++ pywdcollections.py
@@ -12,42 +12,45 @@ import time
 
 class Collection:
     def __init__(self):
-        self.query = ''
-        self.properties = []
+        if not (self.db and self.name and self.properties and self.query):
+            print("Please define your collection's DB, name, query and properties first.")
+        self.db.cur.execute('CREATE TABLE IF NOT EXISTS %s (wikidata_id, %s)' % (self.name, ','.join(['P%s' % prop for prop in self.properties])))
 
     def fetch(self, filepath):
         url = 'https://wdq.wmflabs.org/api'
-        if self.query and self.properties:
-            if filepath and os.path.isfile(filepath) and Utils.fileage(filepath) < 3600: # Only fetch one time per hour.
-                print('Fetching JSON from cache...')
-                cache_file = open(filepath, 'r')
-                content = cache_file.read().replace('\\\\', '\\')
-                cache_file.close()
-                data = json.loads(content)
-            else:
-                print('Fetching JSON from WikiDataQuery...')
-                params = {'q': self.query, 'props': ','.join(self.properties)}
-                try:
-                    response = requests.get(url, params=params)
-                    data = json.loads(response.text)
-                    if filepath:
-                        cache_file = open(filepath, 'w')
-                        cache_file.write(response.text)
-                        cache_file.close()
-                except Exception as e:
-                    data = None
-                    print('Fetching failed:', e)
-                    return
-            if 'status' in data.keys() and 'items' in data['status'].keys():
-                print(data['status']['items'], 'elements loaded')
-            if 'props' in data.keys():
-                for prop in self.properties:
-                    if prop in data['props'].keys():
-                        print(len(data['props'][prop]), 'claims for property', prop)
-                    else:
-                        print('No claim for property', prop)
+        if filepath and os.path.isfile(filepath) and Utils.fileage(filepath) < 3600: # Only fetch one time per hour.
+            print('Fetching JSON from cache...')
+            cache_file = open(filepath, 'r')
+            content = cache_file.read().replace('\\\\', '\\')
+            cache_file.close()
+            data = json.loads(content)
         else:
-            print("Please define your collection's query and properties before fetching.")
+            print('Fetching JSON from WikiDataQuery...')
+            params = {'q': self.query, 'props': ','.join(self.properties)}
+            try:
+                response = requests.get(url, params=params)
+                data = json.loads(response.text)
+                if filepath:
+                    cache_file = open(filepath, 'w')
+                    cache_file.write(response.text)
+                    cache_file.close()
+            except Exception as e:
+                data = None
+                print('Fetching failed:', e)
+                return
+        if 'status' in data.keys() and 'items' in data['status'].keys():
+            print(data['status']['items'], 'elements loaded')
+            for wikidata_id in data['items']:
+                self.db.cur.execute('INSERT OR IGNORE INTO %s (wikidata_id) VALUES (?)' % (self.name,), (wikidata_id,))
+        if 'props' in data.keys():
+            for prop in self.properties:
+                if prop in data['props'].keys():
+                    print(len(data['props'][prop]), 'claims for property', prop)
+                    for (wikidata_id, dtype, value) in data['props'][prop]:
+                        self.db.cur.execute('UPDATE %s SET P%s = ? WHERE wikidata_id = ?' % (self.name, prop), (value, wikidata_id))
+                else:
+                    print('No claim for property', prop)
+            self.db.con.commit()
 
 class Database:
     def __init__(self, filepath):
